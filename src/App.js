ecf27febb26dc1beb0a713bc157f5c4d3f2caf11
Naman Bhardwaj <namanbhardwaj.official@gmail.com>
2024-10-17 22:35:20 +0530

    removing the code which is not needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const vipSeat = new Array(25).fill(0);
+  for (let i = 0; i < vipSeat.length; i++) {
+    vipSeat[i] = `vipSeat${i + 1}`;
+  }
+  const economySeat = new Array(36).fill(0);
+  for (let i = 0; i < economySeat.length; i++) {
+    economySeat[i] = `economySeat${i + 1}`;
+  }
+
+  const regularSeat = new Array(49).fill(0);
+  for (let i = 0; i < regularSeat.length; i++) {
+    regularSeat[i] = `regularSeat${i + 1}`;
+  }
+
+  const [seatsSelected, SetseatsSelected] = useState([]);
+
+  const checkifSelectionPossible = (seatsSelected) => {
+    return seatsSelected.length < 5;
+  };
+
+  const bookTicketForConcert = (seat) => {
+    if (checkifSelectionPossible(seatsSelected)) {
+      if (!seatsSelected.includes(seat)) {
+        SetseatsSelected((prevSeat) => [seat, ...prevSeat]);
+      } else {
+        SetseatsSelected((prevSeat) =>
+          prevSeat.filter((seatRemove) => seatRemove !== seat)
+        );
+      }
+    } else {
+      alert(`You have already booked the 5 seats for a user"${seatsSelected}`);
+    }
+  };
+  const clearSeats = () => {
+    SetseatsSelected([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Concert Seat Selection</h1>
+      <h2>
+        Seats Booked {seatsSelected.length}: {seatsSelected.join(",  ")}
+      </h2>
+      <h3 onClick={() => clearSeats()}>Clear All Seats</h3>
+      <div className="concert">
+        {vipSeat.map((seat) => {
+          return (
+            <div
+              key={seat}
+              className="vip-seat"
+              onClick={() => bookTicketForConcert(seat)}
+            >
+              {seat}
+            </div>
+          );
+        })}
+      </div>
+      <div className="concert">
+        {economySeat.map((economySeat) => {
+          return (
+            <div
+              key={economySeat}
+              className="economy-seat"
+              onClick={() => bookTicketForConcert(economySeat)}
+            >
+              {economySeat}
+            </div>
+          );
+        })}
+      </div>
+      <div className="concert">
+        {regularSeat.map((regularSeat) => {
+          return (
+            <div
+              key={regularSeat}
+              className="regular-seat"
+              onClick={() => bookTicketForConcert(regularSeat)}
+            >
+              {regularSeat}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
